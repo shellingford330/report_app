9bfbf812c36a55dbd69c6b609d8b53ba4651b0da
Hirofumi <pg.econ4869@gmail.com>
2019-01-05 06:32:30 +0900

    生徒登録とエラー・サクセスメッセージを追加

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,37 @@
 class StudentsController < ApplicationController
+	before_action :set_student, only: [:show, :edit, :update, :destroy]
 	def index
-		
+		@students = Student.all
 	end
+
+	def	new
+		@student = Student.new
+	end
+
+	def	show
+	end
+
+	def create
+		@student = Student.new(student_params)
+		if @student.save
+			flash[:success] = "無事作成されました！"
+			redirect_to @student
+		else
+			flash.now[:danger] = "もう一度作成し直してください。"
+			render 'new'
+		end
+	end
+
+	def destroy
+		@student.destroy
+		redirect_to students_path
+	end
+	private
+		def	set_student
+			@student = Student.find(params[:id])
+		end
+
+		def	student_params
+			params.require(:student).permit(:name, :email, :password, :password_confirmation)
+		end
 end
