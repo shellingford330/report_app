eb10b50221c4512fac1528daaf6bcc5c66135237
suzuki pri <pg.econ4869@gmail.com>
2019-05-11 13:57:58 +0900

    お問い合わせのビューリファクタリング

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,24 @@
 module ContactsHelper
+
+	# 未読確認
+
+	# 生徒側が講師からの返信を確認していればtrue, していなければfalseを返す
+	def student_checked?(contact)
+		contact.replies.where(writeable_type: "Teacher", read_flg: false).exists?
+	end
+
+	# 講師側が生徒からのお問い合わせや返信を確認していればtrue, していなければfalseを返す
+	def	teacher_checked?(contact)
+		!contact.read_flg || contact.replies.where(writeable_type: "Student", read_flg: false).exists?
+	end
+
+	
+	#返信有無確認
+
+	# 講師から返信があるか確認
+	def	reply_exists?(contact)
+		contact.replies.where(writeable_type: "Teacher").exists?
+	end
+
+
 end
