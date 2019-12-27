03e1a521b7459aacc1b51fb0e48fe2a172a1827d
suzuki pri <pg.econ4869@gmail.com>
2019-12-27 21:41:55 +0900

    お知らせの添付ファイルバグ

--- app/models/concerns/file_validation.rb
+++ app/models/concerns/file_validation.rb
@@ -6,11 +6,7 @@ module FileValidation
 	private
 		def file_invalid?
 			if self.upfile?
-				name = self.upfile.file.original_filename
-				perms = ['.jpg', '.jpeg', '.gif', '.png', '.xlsx', '.pdf', '.pptx', '.docx']
-				if !perms.include?(File.extname(name).downcase)
-					errors.add(:base, 'アップロード可能な種類のファイルではありません')
-				elsif self.upfile.file.size > 5.megabyte
+				if self.upfile.size > 5.megabyte
 					errors.add(:base, 'ファイルサイズは5MBサイズまでです')
 				end
 			end
