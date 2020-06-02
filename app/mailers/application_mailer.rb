9525540f14af1b062b11ed2cb1efc451bb4945a4
suzuki pri <pg.econ4869@gmail.com>
2020-06-02 12:10:43 +0900

    Domainを変更

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: '"自由塾" <jiyujyuku@okurun.co.jp>'
+  default from: '"自由塾" <jiyujyuku@okurun.jp>'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'www.okurun.co.jp'
+  host = 'okurun.jp'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
