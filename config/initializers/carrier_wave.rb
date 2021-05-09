a1d104722374e1d7371d04f9e4a869284aec98af
suzuki pri <pg.econ4869@gmail.com>
2021-05-09 18:02:33 +0900

    fix: typo

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,6 +8,6 @@ if Rails.env.production?
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-    config.fog_public        = fase
+    config.fog_public        = false
   end
 end
